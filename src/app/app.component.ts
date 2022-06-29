e025a0aba42cb87d937f0c25787d6dd86f776bd4
Danyil <demyanyuk76@gmail.com>
2022-06-29 17:16:49 +0300

    uploaded project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatDialog} from '@angular/material/dialog';
+import { DialogComponent } from './components/dialog/dialog.component';
+import { ApiService } from './services/api.service';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {MatTableDataSource} from '@angular/material/table';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MarketStorage';
+  displayedColumns: string[] = ['productName', 'category', 'date', 'freshness', 'price', 'comment', 'action'];
+  dataSource !: MatTableDataSource<any>;
+
+  @ViewChild(MatPaginator) paginator !: MatPaginator;
+  @ViewChild(MatSort) sort !: MatSort;
+
+  constructor(private dialog : MatDialog, private api : ApiService){
+  }
+  ngOnInit(): void {
+    this.getAllProducts();
+  }
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent, {
+      width: "50%"
+    }).afterClosed().subscribe(value => {
+      if(value === 'save'){
+        this.getAllProducts()
+      }
+    });
+  }
+  getAllProducts(){
+    this.api.getProduct()
+    .subscribe({
+      next: (response) => {
+        this.dataSource = new MatTableDataSource(response);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (err) => {
+        alert("Error while fetching products")
+      }
+    })
+  }
+  editProduct(row: any){
+    this.dialog.open(DialogComponent, {
+      data: row
+    }).afterClosed().subscribe(value => {
+      if(value === 'update'){
+        this.getAllProducts();
+      }
+    })
+  }
+  deleteProduct(id : number){
+    this.api.deleteProducr(id)
+    .subscribe({
+      next: (response) => {
+        alert("Succesfully deleted!")
+        this.getAllProducts();
+      },
+      error: () => {
+        alert("Error while deleting product!")
+      }
+    })
+  }
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
 }
---
+++ src/app/components/dialog/dialog.component.ts
@@ -0,0 +1,71 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { ApiService } from 'src/app/services/api.service';
+
+@Component({
+  selector: 'app-dialog',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.scss']
+})
+export class DialogComponent implements OnInit {
+  freshnessList = ["Brand New", "Second Hand", "Refrubished"];
+  productForm !: FormGroup;
+  actionBtn : string = "Save"
+  constructor(private formBuilder : FormBuilder, 
+    private api : ApiService, private dialogRef : MatDialogRef<DialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public editData: any,) { }
+
+  ngOnInit(): void {
+    this.productForm = this.formBuilder.group({
+      productName: ['', Validators.required],
+      category : ['', Validators.required],
+      freshness : ['', Validators.required],
+      price : ['', Validators.required],
+      comment : ['', Validators.required],
+      date : ['', Validators.required],
+    });
+    if(this.editData){
+      this.actionBtn = "Update";
+      this.productForm.controls['productName'].setValue(this.editData.productName);
+      this.productForm.controls['category'].setValue(this.editData.category);
+      this.productForm.controls['freshness'].setValue(this.editData.freshness);
+      this.productForm.controls['price'].setValue(this.editData.price);
+      this.productForm.controls['comment'].setValue(this.editData.comment);
+      this.productForm.controls['date'].setValue(this.editData.date);
+    }
+  }
+  addProduct(){
+    if(!this.editData){
+      if(this.productForm.valid){
+        this.api.postProduct(this.productForm.value)
+        .subscribe({
+          next: (response) => {
+            alert("Product added succesfully");
+            this.productForm.reset();
+            this.dialogRef.close('save');
+          },
+          error: () => {
+            alert("Error while adding the product")
+          }
+        })
+      }
+    } else {
+      this.updateProduct()
+    }
+  }
+  updateProduct(){
+    this.api.updateProduct(this.productForm.value, this.editData.id)
+    .subscribe({
+      next: (response) => {
+        alert("Product updated!")
+        this.productForm.reset();
+        this.dialogRef.close('update');
+      },
+      error: () => {
+        alert("Error while updating product!")
+      }
+    })
+  }
+
+}
